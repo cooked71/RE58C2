c6785e5be5adc58c8221faf98edf22d8c5697415
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-03-02 17:51:11 +0100

    Update twrp_RE58C2.mk

--- twrp_RE58C2.mk
+++ twrp_RE58C2.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from RE58C2 device
 $(call inherit-product, device/realme/RE58C2/device.mk)
