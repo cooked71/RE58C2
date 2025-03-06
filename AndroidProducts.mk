56d43aee3666b8e93084f2fd9c5b41961b33d9ad
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-03-06 23:36:02 +0000

    twrp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,9 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_RE58C2.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_RE58C2-user \
-    twrp_RE58C2-userdebug \
-    twrp_RE58C2-eng
+    twrp_RE58C2-eng \
+    twrp_RE58C2-userdebug
+
+PRODUCT_PACKAGES += \
+    recovery
--- device.mk
+++ device.mk
@@ -1,55 +1,55 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+DEVICE_PATH := device/realme/RE58C2
 
-LOCAL_PATH := device/realme/RE58C2
-# A/B
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/recovery/root/system/etc/twrp.flags:system/etc/twrp.flags
+    
+#sepolicy    
+    BOARD_SEPOLICY_PREBUILT := device/realme/RE58C2/sepolicy/precompiled/precompiled_sepolicy
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 32
+BOARD_SYSTEMSDK_VERSIONS := 32
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
+    boot \
     dtbo \
-    vendor_dlkm \
     system \
     product \
-    system_ext \
-    vbmeta \
     vendor \
-    vbmeta_system \
     odm \
-    vbmeta_vendor \
-    boot
+    odm_dlkm \
+    vbmeta \
+    vendor_boot \
+    vendor_dlkm \
+    vbmeta_system \
+    vbmeta_vendor
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    checkpoint_gc
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.ums9230 \
-    bootctrl.ums9230.recovery \
-    android.hardware.boot@1.0-service
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
 
-PRODUCT_PACKAGES += \
-    bootctrl
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
- # Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true   
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
-
-    # Soong namespaces
+# Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
+# Brightness
+TW_MAX_BRIGHTNESS := 4095
+TW_DEFAULT_BRIGHTNESS := 3095
